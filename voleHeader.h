6416e51754c5f36c625b85529721428c6230038d
AlyNokrashy <ali.ahmed.elnokrashy@gmail.com>
2024-11-03 03:27:44 +0200

    Update Vole.cpp and voleHeader.h

--- voleHeader.h
+++ voleHeader.h
@@ -1,41 +1,78 @@
 #ifndef TASK_3_VOLEHEADER_H
 #define TASK_3_VOLEHEADER_H
 #include <bits/stdc++.h>
-class Memory;
-class Machine;
-class  Storage;
-class Register;
 using namespace std;
-class Machine{
-public:
-    vector<string> readProgram(const string& filename);
-    void setMemory(Memory &memory, vector<string> &instructions);
-};
-class Storage{
+
+string decimal_to_base(int value, int base);
+int base_to_decimal(string value, int base);
+int base_to_decimal(char c);
+
+class Memory_Cell
+{
 protected:
-    map<string, string> cells;
-    int size;
-    Storage(int size): size(size) {}
+    int value{0};
 
 public:
-    void initialize(int wdth);
-    void print();
-    virtual void setCell(const string &address, const string &content) {cells[address] = content;};
-    virtual string& getCell(const string &address) {return cells[address];};
-};
-class Memory : public Storage{
-public:
-    Memory() : Storage(256) {
-        initialize(2);
-    }
-
-};
-class Register : public Storage{
-public:
-    Register() : Storage(16){
-        initialize(1);
-    }
+    Memory_Cell(int val = 0);
+    void set_value(int val);
+    int get_value() const;
+    string binary_value() const;
+    string hex_value() const;
+    int twos_comp_value() const;
+    double float_value() const;
 };
 
-vector<string> split(string target, string delimiter);
+class Register: public Memory_Cell
+{
+public:
+    bool operator==(const Memory_Cell &rhs) const;
+    bool operator!=(const Memory_Cell &rhs) const;
+    Register operator=(const Memory_Cell &rhs);
+    Register operator=(const int rhs);
+    Register operator++();
+    Register operator+=(const Register &rhs);
+    Register operator+=(const int rhs);
+};
+
+class Arithmetic_Unit
+{
+public:
+    string float_to_binary(double d);
+    int add_int(int val1, int val2);
+    double add_float(double val1, double val2);
+};
+
+class Machine
+{
+    Arithmetic_Unit AU;
+    Memory_Cell *Memory;
+    Register *Register, ProgramCtr, InstructionCtr;
+    int rCount, mSize;
+    bool halt;
+    string screen;
+
+    bool valid_value(string &ins);
+    void __1(string &ins);
+    void __2(string &ins);
+    void __3(string &ins);
+    void __4(string &ins);
+    void __5(string &ins);
+    void __6(string &ins);
+    void __B(string &ins);
+
+public:
+    Machine(int memory_size = 256, int register_count = 16);
+
+    int registerCount();
+    int memorySize();
+    bool run_one_cycle();
+    string sceen();
+    Memory_Cell &atM(int index);
+    Register &atR(int index);
+    void reset();
+    string PC();
+    string IR();
+    bool halted();
+    ~Machine();
+};
 #endif //TASK_3_VOLEHEADER_H
